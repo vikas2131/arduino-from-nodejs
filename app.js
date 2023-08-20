c9e3e33fd8dee77929a90b137db621b347fa37a7
Vikas Pal <vikaspal2131@gmail.com>
2023-08-20 19:56:51 +0530

    v2

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
 var http = require('http');
-
 var fs = require('fs');
 var index = fs.readFileSync( 'index.html');
-
-var SerialPort = require('serialport');
-const parsers = SerialPort.parsers;
-
-const parser = new parsers.Readline({
-    delimiter: '\r\n'
+const io = require("socket.io")(3000, {
+    cors:{
+        origin: ["http://localhost:3002"],
+    },
 });
 
-var port = new SerialPort('/dev/tty.usbmodem101',{ 
-    baudRate: 9600,
-    dataBits: 8,
-    parity: 'none',
-    stopBits: 1,
-    flowControl: false
-});
-
-port.pipe(parser);
+io.on("connection", socket => {
+    console.log(socket.id)
+})
 
 var app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(index);
 });
 
-var io = require('socket.io').listen(app);
+var io2 = require('socket.io')(app);
 
-io.on('connection', function(socket) {
+io2.on('connection', function(socket) {
     
     socket.on('lights',function(data){
         
         console.log( data );
         
-        port.write( data.status );
+        io.emit('lights', data);
     
     });
     
 });
 
-app.listen(3000);
+app.listen(3001);
